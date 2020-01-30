4ea10b06eca4b04d1369b130c78d3579c74f05b7
Dale Larson <dlarson42@gmail.com>
2020-01-30 13:19:08 -0600

    Add support for sabnzbd.

--- run-ansible.sh
+++ run-ansible.sh
@@ -2,6 +2,6 @@
 
 # Run the playbook from within ansible-nas repository.
 pushd ansible-nas
-ansible-playbook -i ../inventories/localhost/inventory ../my-nas.yml nas.yml -b -K --ask-vault-pass
+ansible-playbook -i ../inventories/localhost/inventory ../my-nas.yml nas.yml -b -K --ask-vault-pass $@
 popd
 
